1718696b8cbc19afd494c46af425633e54c9d125
Kevin Bjørn <73933092+KevinDania@users.noreply.github.com>
2024-05-27 23:54:52 +0200

    material ui testing and button hover added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import { AppBar, Box, Toolbar, Typography, Button, ButtonGroup } from "@mui/material";
-
+import { useState } from "react";
 
 function App() {
-  return (
-    <div>
-      <Box>
-        <AppBar>
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              myWebsite
-            </Typography>
-            <ButtonGroup variant="text" color="inherit">
-              <Button color="inherit">section 1</Button>
-              <Button color="inherit">section 2</Button>
-              <Button color="inherit">section 3</Button>
-            </ButtonGroup>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+  const [hover1, setHover1] = useState(false);
+  const [hover2, setHover2] = useState(false);
+  const [hover3, setHover3] = useState(false);
 
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </div>
+  return (
+    <Box>
+      <AppBar sx={{ backgroundColor: "#303030" }}>
+        <Toolbar sx={{ justifyContent: "center" }}>
+          <Typography variant="h6" component="div" >
+            myWebsite
+          </Typography>
+          <ButtonGroup variant="text" color="inherit" >
+          <Button
+              onMouseEnter={() => setHover1(true)}
+              onMouseLeave={() => setHover1(false)}
+              sx={{color: hover1 ? "gray" : null }}
+              >section 1
+            </Button>
+            <Button
+              onMouseEnter={() => setHover2(true)}
+              onMouseLeave={() => setHover2(false)}
+              sx={{color: hover2 ? "gray" : null }}
+              >section 2
+            </Button>
+            <Button
+              onMouseEnter={() => setHover3(true)}
+              onMouseLeave={() => setHover3(false)}
+              sx={{color: hover3 ? "gray" : null }}
+              >section 3
+            </Button>
+          </ButtonGroup>
+
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
